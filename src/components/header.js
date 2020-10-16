5bb6971e5b00dc00f4726d00416121ed81d17f71
Luca Tropea <lucatroinfo@gmail.com>
2020-10-16 11:18:16 +0200

    First commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './header.scss'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>head</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/pages/contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+const Contact = () => {
+  return (
+    <Layout>
+        <h1>Contact page</h1>
+        <Link to="/">Homepage</Link>
+    </Layout>
+  )
+}
+
+export default Contact
